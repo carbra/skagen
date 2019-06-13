66d4b26e01375bf146e84155e34e03ac6b5b3e50
carbra <47956143+carbra@users.noreply.github.com>
2019-06-12 19:26:39 -0500

    espero funcione

--- src/mongoose/controller.js
+++ src/mongoose/controller.js
@@ -1,20 +1,17 @@
 const mongoose = require('mongoose');
 
-const mongo_uri = "mongodb+srv://skagenUsuario:skagen2019@cluster0-aog9o.mongodb.net/test?retryWrites=true"
+// const mongo_uri = "mongodb+srv://skagenUsuario:skagen2019@cluster0-aog9o.mongodb.net/test?retryWrites=true"
+console.log(process.env.MONGODB_URI)
 
-//const mongo_uri = "mongodb+srv://SkagenN:skagenskagen@cluster0-cparl.mongodb.net/test?retryWrites=true"
-// const mongo_uri =" mongodb+srv://AutosolutionsSK:skagen.2019@cluster0-cparl.mongodb.net/test?retryWrites=true"
-//const mongo_uri = "mongodb+srv://testUser:TestUser@cluster0-ib10n.mongodb.net/Articulos?retryWrites=true"
-
-mongoose.connect(
-    mongo_uri,
-    { useNewUrlParser: true },
-    (err) => {
-        return err 
-            ? console.error(`!!! Error al intentar conectar con el cluster!!!\n${err}`)
-            : console.log(' --- ¡Conexión exitosa con Mongo Atlas! ---');
-    }
-);
+// mongoose.connect(
+//     process.env.MONGO_URI,
+//     { useNewUrlParser: true },
+//     (err) => {
+//         return err 
+//             ? console.error(`!!! Error al intentar conectar con el cluster!!!\n${err}`)
+//             : console.log(' --- ¡Conexión exitosa con Mongo Atlas! ---');
+//     }
+// );
 
 
 
